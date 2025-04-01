9e21f577a71fc63b175d9a60fc9451e9700af37e
PedroCLH2 <pewhenriquegamer@gmail.com>
2025-03-31 22:13:07 -0300

    Repondo projetos

--- Java/Aula02/src/Principal.java
+++ Java/Aula02/src/Principal.java
@@ -1,37 +1,70 @@
 import java.util.Scanner;
 
 public class Principal {
+
     public static void main(String[] args) {
-    //Criar o objeto responsável por ler os dados digitados no teclado
-
+        //Criar o objeto responsável por ler os dados digitados no teclado
         Scanner leitor = new Scanner(System.in);
-        Tenis tenis1 = new Tenis();
 
-        //Criar um objeto tenis com os valores digitados eplo usuários
-        System.out.println("Digite o modelo do produto");
-        String modelo = leitor.next() + leitor.nextLine(); //Le a string com mais
+        //Criar um objeto tenis com os valores digitados pelo usuário
+        System.out.println("Digite o modelo do tenis");
+        String modelo = leitor.next() + leitor.nextLine(); //Le a string com mais de 1 palavra
 
-        System.out.println("Digite a cor do seu tênis");
+        System.out.println("Digite a cor");
         String cor = leitor.next() + leitor.nextLine();
 
-
-
-        System.out.println("Digite o tamanho desejado");
+        System.out.println("Digite o tamanho do tenis");
         int tamanho = leitor.nextInt();
 
         System.out.println("Digite o valor");
-         double preco = leitor.nextDouble();
+        double valor = leitor.nextDouble();
 
+        System.out.println("Está disponível? true/false");
         boolean disponivel = leitor.nextBoolean();
-      //Colocar os valores lidos dentro do objeto
+
+        //Ler as informações da marca
+        System.out.println("Digite o nome da marca");
+        String nomeMarca = leitor.next() + leitor.nextLine();
+
+        System.out.println("Insira o nome do arquivo da logo");
+        String logo = leitor.next() + leitor.nextLine();
+
+        //Criar o objeto marca
+        Marca marca = new Marca();
+
+        //Atribuir os valores ao objeto
+        marca.nome = nomeMarca;
+        marca.logo = logo;
+
+        //Criar o objeto Tenis
+        Tenis tenis1 = new Tenis();
+        //Colocar os valores lidos dentro do objeto
         tenis1.modelo = modelo;
         tenis1.cor = cor;
         tenis1.disponivel = disponivel;
         tenis1.tamanho = tamanho;
-        tenis1.preco = preco;
+        tenis1.preco = valor;
+        tenis1.marca = marca;
 
         //Exibir os valores que estão dentro do objeto
+        System.out.println("Modelo: " + tenis1.modelo);
+        System.out.println("Cor: " + tenis1.cor);
+        System.out.println("Disponível: " + tenis1.disponivel);
+        System.out.println("Tamanho: " + tenis1.tamanho);
+        System.out.println("Preço: " + tenis1.preco);
+        //Exibir os dados da marca (através do objeto tenis)
+        System.out.println("Marca: " + tenis1.marca.nome);
+        System.out.println("Logo: " + tenis1.marca.logo);
 
+        //Chamar (invocar) o método para aumentar o valor do tenis em 15
+        tenis1.aumentarPreco(10);
+
+        //Exibir o valor atualizado do tenis
+        System.out.println("Valor atualizado: " + tenis1.preco);
+
+        //Chamar o método para calcular o proço com o desconto
+        double valorDoTenisComDesconto = tenis1.aplicarDesconto(50);
+        System.out.println("Tenis com desconto: " + valorDoTenisComDesconto);
 
 
 
--- Java/Aula02/src/Tenis.java
+++ Java/Aula02/src/Tenis.java
@@ -1,9 +1,25 @@
 public class Tenis {
 
-        String cor;
-        String modelo;
-        int tamanho;
-        Double preco;
-        boolean disponivel;
+    //Atributos
+    String cor;
+    String modelo;
+    int tamanho;
+    double preco;
+    boolean disponivel;
+
+    //Atributo de referencia (outra classe que iremos criar)
+    Marca marca;
+
+    //Métodos
+    //Método para aumentar o preço do tenis em porcentagem (10, 20)
+    void aumentarPreco(float porcentagem){
+        preco += preco * porcentagem/100; // preco = preco + preco*porcentagem/100;
+    }
+
+    double aplicarDesconto(float porcentagem){
+        double valorComDesconto;
+        valorComDesconto = preco - (preco*porcentagem)/100;
+        return valorComDesconto;
+    }
 
 }
